5057ace41dbaa44eede8de1375edc2566c70440b
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:25:53 -0300

    Create: Data Class - BussinesCard (data.entity)

---
+++ app/src/main/java/com/study/kotlin/businesscard/data/entity/BussinesCard.kt
@@ -0,0 +1,15 @@
+package com.study.kotlin.businesscard.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class BussinesCard (
+
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+    val name:String,
+    val company:String,
+    val phone:String,
+    val email:String,
+    val background: String,
+)
