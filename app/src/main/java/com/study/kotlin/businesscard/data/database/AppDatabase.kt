b1ab71513f6629e7e11d9fdffbdf5f9d8fbfe34a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:43:02 -0300

    Create: Database - AppDatabase (data.database)

--- app/src/main/java/com/study/kotlin/businesscard/data/database/AppDatabase.kt
+++ app/src/main/java/com/study/kotlin/businesscard/data/database/AppDatabase.kt
@@ -1,4 +1,13 @@
 package com.study.kotlin.businesscard.data.database
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.study.kotlin.businesscard.data.database.dao.BusinessCardDAO
+import com.study.kotlin.businesscard.data.entity.BusinessCard
+
+@Database(entities = [BusinessCard::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+  abstract fun businessCardDAO(): BusinessCardDAO
+
 }
