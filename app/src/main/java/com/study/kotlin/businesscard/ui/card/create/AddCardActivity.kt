e33dd2646582c0dfa85a7643638d1c56a5cd4372
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 16:04:48 -0300

    Add: SVG - ic_close_black_24dp (drawable)

--- app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
@@ -2,6 +2,7 @@ package com.study.kotlin.businesscard.ui.card.create
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.study.kotlin.businesscard.R
 
 class AddCardActivity : AppCompatActivity() {
 
