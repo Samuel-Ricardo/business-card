f090ab6c4315c44a790640a5bf5a8ff0ad18de76
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:47:35 -0300

    Create: Room Database Instance - AppDatabase (data.database)

--- app/src/main/java/com/study/kotlin/businesscard/data/database/AppDatabase.kt
+++ app/src/main/java/com/study/kotlin/businesscard/data/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.study.kotlin.businesscard.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.study.kotlin.businesscard.data.database.dao.BusinessCardDAO
 import com.study.kotlin.businesscard.data.entity.BusinessCard
@@ -10,4 +12,30 @@ abstract class AppDatabase: RoomDatabase() {
 
   abstract fun businessCardDAO(): BusinessCardDAO
 
+  companion object{
+
+    // Singleton prevents multiple instances of database opening at the
+    // same time.
+
+    @Volatile
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context):AppDatabase {
+
+      // if the INSTANCE is not null, then return it,
+      // if it is, then create the database
+
+      return INSTANCE ?: synchronized(this) {
+        val instance = Room.databaseBuilder(
+          context.applicationContext,
+          AppDatabase::class.java,
+          "businesscard_db"
+        ).build()
+        INSTANCE = instance
+
+        // return instance
+        instance
+      }
+    }
+  }
 }
