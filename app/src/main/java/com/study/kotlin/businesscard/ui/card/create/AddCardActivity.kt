d1f4bea17ab2ed3128a1b8f63c633db2a2dd7e27
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 17:40:56 -0300

    Refactor: Rename (View)

--- app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
@@ -3,11 +3,14 @@ package com.study.kotlin.businesscard.ui.card.create
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.study.kotlin.businesscard.R
+import com.study.kotlin.businesscard.databinding.ActivityAddCradBinding
 
 class AddCardActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityAddCardBinding }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_crad)
+        setContentView(R.layout.activity_add_card)
     }
 }
