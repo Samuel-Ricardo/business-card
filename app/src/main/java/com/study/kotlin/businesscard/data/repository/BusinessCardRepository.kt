b425149436f69236995f49995dfd5e47bfe5c433
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:36:54 -0300

    Create: Function - insert (data.repository.BusinessCardRepository)

--- app/src/main/java/com/study/kotlin/businesscard/data/repository/BusinessCardRepository.kt
+++ app/src/main/java/com/study/kotlin/businesscard/data/repository/BusinessCardRepository.kt
@@ -1,8 +1,17 @@
 package com.study.kotlin.businesscard.data.repository
 
 import com.study.kotlin.businesscard.data.database.dao.BusinessCardDAO
+import com.study.kotlin.businesscard.data.entity.BusinessCard
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class BusinessCardRepository(private val DAO: BusinessCardDAO) {
 
+  fun insert(businessCard: BusinessCard) = runBlocking {
+    launch(Dispatchers.IO){
+      DAO.insert(businessCard)
+    }
+  }
 
 }
