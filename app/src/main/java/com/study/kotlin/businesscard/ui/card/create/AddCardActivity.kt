8ca4da99a97e56d53a2b1898cd7ed0167290c0d8
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 15:01:49 -0300

    Create: Package - main - (ui)

---
+++ app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
@@ -0,0 +1,12 @@
+package com.study.kotlin.businesscard.ui.card.create
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCrad : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_crad)
+    }
+}
