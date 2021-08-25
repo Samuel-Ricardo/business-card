873ee761124773755748f68805cfbc024ad71c8d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:34:37 -0300

    Create: Repository - BusinessCardRepository (data.repository)

---
+++ app/src/main/java/com/study/kotlin/businesscard/data/repository/BusinessCardRepository.kt
@@ -0,0 +1,8 @@
+package com.study.kotlin.businesscard.data.repository
+
+import com.study.kotlin.businesscard.data.database.dao.BusinessCardDAO
+
+class BusinessCardRepository(private val DAO: BusinessCardDAO) {
+
+
+}
