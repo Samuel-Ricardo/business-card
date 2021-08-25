3fb3fff0004ecf1c55bef8dd5b450a576e78583f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:32:40 -0300

    Create: DAO - BusinessCardDAO (data.database.dao)

--- app/src/main/java/com/study/kotlin/businesscard/data/database/dao/BusinessCardDAO.kt
+++ app/src/main/java/com/study/kotlin/businesscard/data/database/dao/BusinessCardDAO.kt
@@ -1,4 +1,18 @@
 package com.study.kotlin.businesscard.data.database.dao
 
-class BusinessCardDAO {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.study.kotlin.businesscard.data.entity.BusinessCard
+
+@Dao
+interface BusinessCardDAO {
+
+  @Query("SELECT * FROM BusinessCard")
+  fun getAll(): LiveData<List<BusinessCard>>
+
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  suspend fun insert(businessCard: BusinessCard)
 }
