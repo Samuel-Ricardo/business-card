5f8bb17f99a9e221f88972f1fcad8f81e0d80a82
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 17:43:28 -0300

    Setup: Binding - AddCardActivity (ui.card.create)

--- app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/card/create/AddCardActivity.kt
@@ -3,14 +3,15 @@ package com.study.kotlin.businesscard.ui.card.create
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.study.kotlin.businesscard.R
-import com.study.kotlin.businesscard.databinding.ActivityAddCradBinding
+import com.study.kotlin.businesscard.databinding.ActivityAddCardBinding
 
 class AddCardActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityAddCardBinding }
+    private val binding by lazy { ActivityAddCardBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_card)
+        setContentView(binding.root)
+        setup()
     }
 }
