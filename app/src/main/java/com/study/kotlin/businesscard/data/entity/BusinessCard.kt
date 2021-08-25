a08c0457776ed2c5c6747ba9cbe282fadc4572f2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 18:29:09 -0300

    Refactor: Rename - BussinesCard - BusinessCard (data.entity)

--- app/src/main/java/com/study/kotlin/businesscard/data/entity/BusinessCard.kt
+++ app/src/main/java/com/study/kotlin/businesscard/data/entity/BusinessCard.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class BussinesCard (
+data class BusinessCard (
 
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name:String,
