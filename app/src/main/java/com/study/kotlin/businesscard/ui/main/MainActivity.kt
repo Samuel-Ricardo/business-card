2bcb7024b121c8e5aa1eee85d7a797f241a27bd9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 17:56:39 -0300

    Create: Function - setuplistener() (ui.card.create.AddCardActivity)

--- app/src/main/java/com/study/kotlin/businesscard/ui/main/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.study.kotlin.businesscard.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.study.kotlin.businesscard.R
 import com.study.kotlin.businesscard.databinding.ActivityMainBinding
+import com.study.kotlin.businesscard.ui.card.create.AddCardActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +23,8 @@ class MainActivity : AppCompatActivity() {
     fun setuplistener() {
 
         binding.fabAdd.setOnClickListener{
-            
+            val intent = Intent(this@MainActivity, AddCardActivity::class.java)
+            startActivity(intent)
         }
     }
 }
