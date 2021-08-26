afe95c750b5499eb05b0c669f6df397832858618
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 23:31:29 -0300

    Override: Fun - create (ui.viewmodel.factory.MainViewModelFactory)

--- app/src/main/java/com/study/kotlin/businesscard/ui/viewmodel/factory/MainViewModelFactory.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/viewmodel/factory/MainViewModelFactory.kt
@@ -3,10 +3,17 @@ package com.study.kotlin.businesscard.ui.viewmodel.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.study.kotlin.businesscard.data.repository.BusinessCardRepository
+import com.study.kotlin.businesscard.ui.viewmodel.MainViewModel
 
 class MainViewModelFactory(private val repository: BusinessCardRepository): ViewModelProvider.Factory {
 
   override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-    TODO("Not yet implemented")
+
+    if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+      @Suppress("UNCHECKED_CAST")
+      return MainViewModel(repository) as T
+    }
+
+    throw IllegalArgumentException("<Me> Unknow ViewModel Class")
   }
 }
