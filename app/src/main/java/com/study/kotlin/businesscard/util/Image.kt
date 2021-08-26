18ec52811e6027e9c694309b08b83b255451f61b
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-26 01:39:35 -0300

    Create: Function - getScreenShotFromView(view) (ui.util.Image)

--- app/src/main/java/com/study/kotlin/businesscard/util/Image.kt
+++ app/src/main/java/com/study/kotlin/businesscard/util/Image.kt
@@ -1,6 +1,9 @@
 package com.study.kotlin.businesscard.util
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.util.Log
 import android.view.View
 
 class Image {
@@ -8,10 +11,32 @@ class Image {
   companion object {
 
     fun share(context: Context, view: View){
-      val bitmap = getScreenChotFromView(view)
+      val bitmap = getScreenShotFromView(view)
       bitmap?.let {
         saveMediaStorage(context, bitmap)
       }
+    }
+
+    private fun getScreenShotFromView(view: View):Bitmap? {
+
+      var screenshot: Bitmap? = null
+
+      try {
+        screenshot = Bitmap.createBitmap(
+          view.measuredWidth,
+          view.measuredHeight,
+          Bitmap.Config.ARGB_8888
+        )
+
+        val canvas = Canvas(screenshot)
+
+        view.draw(canvas)
+
+      }catch (e: Exception) {
+        Log.e("GFG",  "Failed to capture screenshot because: ${e.message}")
+      }
+
+      return screenshot
     }
   }
 
