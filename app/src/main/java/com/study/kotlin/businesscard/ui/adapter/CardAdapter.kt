30f785765eb3a0a17bfa43fad7dcc0492f7eb59e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 22:51:16 -0300

    Create: Function - bind(item) (ui.adapter)

--- app/src/main/java/com/study/kotlin/businesscard/ui/adapter/CardAdapter.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/adapter/CardAdapter.kt
@@ -1,5 +1,6 @@
 package com.study.kotlin.businesscard.ui.adapter
 
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import com.study.kotlin.businesscard.data.entity.BusinessCard
 import com.study.kotlin.businesscard.databinding.CardBinding
@@ -12,6 +13,17 @@ class CardViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
   fun bind(item:BusinessCard) {
-    
+
+    binding.tvCardName.text = item.name
+    binding.tvCardMail.text = item.email
+    binding.tvCardPhone.text = item.phone
+    binding.tvCardEmpresa.text = item.company
+
+    binding.cdContent.setBackgroundColor(
+      Color.parseColor(item.background)
+    )
+    binding.cardview.setOnClickListener {
+      listenerShare(it)
+    }
   }
 }
