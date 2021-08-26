4f430941ef650e683e2b5496b9b3bb6c09a707a2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 22:42:00 -0300

    Create: ViewHolder - CardViewHolder (ui.adapter)

---
+++ app/src/main/java/com/study/kotlin/businesscard/ui/adapter/CardAdapter.kt
@@ -0,0 +1,17 @@
+package com.study.kotlin.businesscard.ui.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.study.kotlin.businesscard.data.entity.BusinessCard
+import com.study.kotlin.businesscard.databinding.CardBinding
+
+class CardAdapter {
+}
+
+class CardViewHolder(
+  private val binding: CardBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+  fun bind(item:BusinessCard) {
+    
+  }
+}
