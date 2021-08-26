0b17a5d4916525ef07d684ef7f689e0d8915df10
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 23:27:45 -0300

    Create: View Model Factory - Main (ui.viewmodel.factory)

--- app/src/main/java/com/study/kotlin/businesscard/ui/viewmodel/factory/MainViewModelFactory.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/viewmodel/factory/MainViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.study.kotlin.businesscard.ui.viewmodel.factory
 
-class MainViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.study.kotlin.businesscard.data.repository.BusinessCardRepository
+
+class MainViewModelFactory(private val repository: BusinessCardRepository): ViewModelProvider.Factory {
+
+  override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    TODO("Not yet implemented")
+  }
 }
