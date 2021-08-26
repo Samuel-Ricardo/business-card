25e6870ef6b467b4a42e25ba707eac050417ca9c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 23:21:11 -0300

    Create: Function - insert (ui.viewmodel.MainViewModel)

--- app/src/main/java/com/study/kotlin/businesscard/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/viewmodel/MainViewModel.kt
@@ -1,11 +1,16 @@
 package com.study.kotlin.businesscard.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.study.kotlin.businesscard.data.entity.BusinessCard
 import com.study.kotlin.businesscard.data.repository.BusinessCardRepository
 
 class MainViewModel (
   private val businessCardRepository: BusinessCardRepository
 ): ViewModel(){
+
+  fun insert(businessCard: BusinessCard) {
+    businessCardRepository.insert(businessCard)
+  }
 
 
 }
