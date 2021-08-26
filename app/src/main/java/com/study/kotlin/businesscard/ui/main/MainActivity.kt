f761b4b473bbe4474f0aa5529a48fe8bab2441f3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 23:41:02 -0300

    Setup: Activity - MainActivity (ui.main)

--- app/src/main/java/com/study/kotlin/businesscard/ui/main/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/businesscard/ui/main/MainActivity.kt
@@ -3,20 +3,34 @@ package com.study.kotlin.businesscard.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.study.kotlin.businesscard.App
 import com.study.kotlin.businesscard.R
 import com.study.kotlin.businesscard.databinding.ActivityMainBinding
+import com.study.kotlin.businesscard.ui.adapter.CardAdapter
 import com.study.kotlin.businesscard.ui.card.create.AddCardActivity
+import com.study.kotlin.businesscard.ui.viewmodel.MainViewModel
+import com.study.kotlin.businesscard.ui.viewmodel.factory.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel:MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { CardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setup()
     }
 
     fun setup(){
+
+        binding.rcCards
         setuplistener()
     }
 
