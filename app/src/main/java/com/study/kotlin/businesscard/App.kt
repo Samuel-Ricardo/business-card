3d940df1bc910f6f78bab31545d2e317710e8c4c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-25 22:37:48 -0300

    Create: Init Database - App (app)

--- app/src/main/java/com/study/kotlin/businesscard/App.kt
+++ app/src/main/java/com/study/kotlin/businesscard/App.kt
@@ -1,6 +1,11 @@
 package com.study.kotlin.businesscard
 
 import android.app.Application
+import com.study.kotlin.businesscard.data.database.AppDatabase
+import com.study.kotlin.businesscard.data.repository.BusinessCardRepository
 
 class App: Application() {
+
+  val database by lazy {AppDatabase.getDatabase(this)}
+  val repository by lazy {BusinessCardRepository(database.businessCardDAO())}
 }
